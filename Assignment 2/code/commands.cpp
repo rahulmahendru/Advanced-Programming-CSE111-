9480cfc8be341508def8078d4bca02e985f577c9
rahulmahendru <32964204+rahulmahendru@users.noreply.github.com>
2020-01-23 12:29:16 -0800

    Assignment 2 added

---
+++ Assignment 2/code/commands.cpp
@@ -0,0 +1,104 @@
+// $Id: commands.cpp,v 1.18 2019-10-08 13:55:31-07 - - $
+
+#include "commands.h"
+#include "debug.h"
+
+command_hash cmd_hash {
+   {"cat"   , fn_cat   },
+   {"cd"    , fn_cd    },
+   {"echo"  , fn_echo  },
+   {"exit"  , fn_exit  },
+   {"ls"    , fn_ls    },
+   {"lsr"   , fn_lsr   },
+   {"make"  , fn_make  },
+   {"mkdir" , fn_mkdir },
+   {"prompt", fn_prompt},
+   {"pwd"   , fn_pwd   },
+   {"rm"    , fn_rm    },
+};
+
+command_fn find_command_fn (const string& cmd) {
+   // Note: value_type is pair<const key_type, mapped_type>
+   // So: iterator->first is key_type (string)
+   // So: iterator->second is mapped_type (command_fn)
+   DEBUGF ('c', "[" << cmd << "]");
+   const auto result = cmd_hash.find (cmd);
+   if (result == cmd_hash.end()) {
+      throw command_error (cmd + ": no such function");
+   }
+   return result->second;
+}
+
+command_error::command_error (const string& what):
+            runtime_error (what) {
+}
+
+int exit_status_message() {
+   int status = exec::status();
+   cout << exec::execname() << ": exit(" << status << ")" << endl;
+   return status;
+}
+
+void fn_cat (inode_state& state, const wordvec& words){
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
+void fn_cd (inode_state& state, const wordvec& words){
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
+void fn_echo (inode_state& state, const wordvec& words){
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+   cout << word_range (words.cbegin() + 1, words.cend()) << endl;
+}
+
+
+void fn_exit (inode_state& state, const wordvec& words){
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+   throw ysh_exit();
+}
+
+void fn_ls (inode_state& state, const wordvec& words){
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
+void fn_lsr (inode_state& state, const wordvec& words){
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
+void fn_make (inode_state& state, const wordvec& words){
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
+void fn_mkdir (inode_state& state, const wordvec& words){
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
+void fn_prompt (inode_state& state, const wordvec& words){
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
+void fn_pwd (inode_state& state, const wordvec& words){
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
+void fn_rm (inode_state& state, const wordvec& words){
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
+void fn_rmr (inode_state& state, const wordvec& words){
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
---
+++ Assignment 2/code/file_sys.cpp
@@ -0,0 +1,118 @@
+// $Id: file_sys.cpp,v 1.7 2019-07-09 14:05:44-07 - - $
+
+#include <iostream>
+#include <stdexcept>
+#include <unordered_map>
+
+using namespace std;
+
+#include "debug.h"
+#include "file_sys.h"
+
+int inode::next_inode_nr {1};
+
+struct file_type_hash {
+   size_t operator() (file_type type) const {
+      return static_cast<size_t> (type);
+   }
+};
+
+ostream& operator<< (ostream& out, file_type type) {
+   static unordered_map<file_type,string,file_type_hash> hash {
+      {file_type::PLAIN_TYPE, "PLAIN_TYPE"},
+      {file_type::DIRECTORY_TYPE, "DIRECTORY_TYPE"},
+   };
+   return out << hash[type];
+}
+
+inode_state::inode_state() {
+   DEBUGF ('i', "root = " << root << ", cwd = " << cwd
+          << ", prompt = \"" << prompt() << "\"");
+}
+
+const string& inode_state::prompt() const { return prompt_; }
+
+ostream& operator<< (ostream& out, const inode_state& state) {
+   out << "inode_state: root = " << state.root
+       << ", cwd = " << state.cwd;
+   return out;
+}
+
+inode::inode(file_type type): inode_nr (next_inode_nr++) {
+   switch (type) {
+      case file_type::PLAIN_TYPE:
+           contents = make_shared<plain_file>();
+           break;
+      case file_type::DIRECTORY_TYPE:
+           contents = make_shared<directory>();
+           break;
+   }
+   DEBUGF ('i', "inode " << inode_nr << ", type = " << type);
+}
+
+int inode::get_inode_nr() const {
+   DEBUGF ('i', "inode = " << inode_nr);
+   return inode_nr;
+}
+
+
+file_error::file_error (const string& what):
+            runtime_error (what) {
+}
+
+const wordvec& base_file::readfile() const {
+   throw file_error ("is a " + error_file_type());
+}
+
+void base_file::writefile (const wordvec&) {
+   throw file_error ("is a " + error_file_type());
+}
+
+void base_file::remove (const string&) {
+   throw file_error ("is a " + error_file_type());
+}
+
+inode_ptr base_file::mkdir (const string&) {
+   throw file_error ("is a " + error_file_type());
+}
+
+inode_ptr base_file::mkfile (const string&) {
+   throw file_error ("is a " + error_file_type());
+}
+
+
+size_t plain_file::size() const {
+   size_t size {0};
+   DEBUGF ('i', "size = " << size);
+   return size;
+}
+
+const wordvec& plain_file::readfile() const {
+   DEBUGF ('i', data);
+   return data;
+}
+
+void plain_file::writefile (const wordvec& words) {
+   DEBUGF ('i', words);
+}
+
+size_t directory::size() const {
+   size_t size {0};
+   DEBUGF ('i', "size = " << size);
+   return size;
+}
+
+void directory::remove (const string& filename) {
+   DEBUGF ('i', filename);
+}
+
+inode_ptr directory::mkdir (const string& dirname) {
+   DEBUGF ('i', dirname);
+   return nullptr;
+}
+
+inode_ptr directory::mkfile (const string& filename) {
+   DEBUGF ('i', filename);
+   return nullptr;
+}
+
