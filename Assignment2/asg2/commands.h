298c1198b4edade79c5a8b48c5248e3a097f0ea9
Rahul Mahendru <ramahend@ucsc.edu>
2020-02-01 20:51:15 -0800

    updates

--- Assignment2/asg2/commands.h
+++ Assignment2/asg2/commands.h
@@ -37,7 +37,6 @@ void fn_prompt (inode_state& state, const wordvec& words);
 void fn_pwd    (inode_state& state, const wordvec& words);
 void fn_rm     (inode_state& state, const wordvec& words);
 void fn_rmr    (inode_state& state, const wordvec& words);
-void fn_comment(inode_state& state, const wordvec& words);
 
 command_fn find_command_fn (const string& command);
 
